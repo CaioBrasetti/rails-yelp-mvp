f12aa6c4b9532ce6127c26f017c9226ff396501d
CaioBrasetti <caiobrasetti@hotmail.com>
2023-11-15 18:27:22 -0300

    Criação da model restaurante e review, feito a ligação entre elas

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Criando............"
+Restaurant.create(name: 'Vintao1', address: 'av paulista, 111', category: 'italian', phone_number: '11 99999-9999')
+Restaurant.create(name: 'Vintao2', address: 'av paulista, 222', category: 'italian', phone_number: '11 99999-9999')
+Restaurant.create(name: 'Vintao3', address: 'av paulista, 333', category: 'italian', phone_number: '11 99999-9999')
+Restaurant.create(name: 'Vintao4', address: 'av paulista, 444', category: 'italian', phone_number: '11 99999-9999')
+Restaurant.create(name: 'Vintao5', address: 'av paulista, 555', category: 'italian', phone_number: '11 99999-9999')
+
+puts "Criado"
